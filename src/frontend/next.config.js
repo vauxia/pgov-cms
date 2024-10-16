7022a3c8711219aee3103d135d9a22d358f10b14
Adrienne <acabouet@protonmail.com>
2024-10-15 22:39:43 -0400

    USWDS setup.

--- src/frontend/next.config.js
+++ src/frontend/next.config.js
@@ -1,8 +1,15 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [process.env.NEXT_IMAGE_DOMAIN],
   },
+  sassOptions: {
+    includePaths: [
+      path.join(__dirname, "node_modules", "@uswds", "uswds", "packages"),
+    ],
+  }
 }
 
 module.exports = nextConfig
--- src/frontend/pages/_app.tsx
+++ src/frontend/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/app"
 
-import "styles/globals.css"
+import "styles/style.scss"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
