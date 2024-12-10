aa37b1dd3ebf893c7a080665a91942a071516967
Daniel <daniel.gading@civicactions.com>
2024-12-10 11:45:19 -0500

    Clean up commented out code

--- src/frontend/components/usa--in-page-nav.tsx
+++ src/frontend/components/usa--in-page-nav.tsx
@@ -2,22 +2,8 @@ import { useState, useEffect } from "react";
 import Link from 'next/link'
 import { FieldLogo } from './field--logo';
 
-// TODO: Add scrollTo code for long pages when using in page navigation.
-
 export function USAInPageNav({links, logo, logoAbove = true}) {
   const [currentLink, setCurrentLink] = useState(links[0].href);
-  // const [scrollTarget, setScrollTarget] = useState(links[0].href);
-  // useEffect(() => {
-  //   if (currentLink !== scrollTarget) {
-  //     setScrollTarget(currentLink);
-  //     let id = currentLink;
-  //     if (id.charAt(0) === "#") {
-  //       id = id.substring(1);
-  //     }
-  //     document.getElementById(id).scrollIntoView({behavior: "smooth"})
-  //   }
-  // }, [currentLink, scrollTarget])
-
   return (
     <aside
       className="usa-in-page-nav"
