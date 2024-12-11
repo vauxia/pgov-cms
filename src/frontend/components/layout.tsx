1d928bc36ddfeaf05c1ebe96d349ce123d844cce
Daniel <daniel.gading@civicactions.com>
2024-12-11 14:39:39 -0500

    PGOV-402: Add mvp banner

--- src/frontend/components/layout.tsx
+++ src/frontend/components/layout.tsx
@@ -1,12 +1,17 @@
+import Link from "next/link";
 import { GovBanner } from "@trussworks/react-uswds";
+import { Alert } from '@trussworks/react-uswds';
 import { PreviewAlert } from "components/preview-alert";
 import { USAHeader } from "./usa--header";
 
 export function Layout({ children }) {
   return (
     <>
-      <GovBanner />
       <PreviewAlert />
+      <GovBanner />
+      <Alert className="margin-top-0" type="info" headingLevel="h4">
+        This is a minimum viable product (MVP) build of the next iteration of performance.gov.
+      </Alert>
       <div className="max-w-screen-md px-6 mx-auto">
         <USAHeader />
         <main className="grid-container margin-bottom-4">{children}</main>
