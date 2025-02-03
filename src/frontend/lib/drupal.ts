ff867a4fb77759b8dade9a84a59e04fb9218e85d
Adrienne <acabouet@protonmail.com>
2025-02-02 22:58:25 -0500

    PGOV-440: Basic auth creds to requesst header for api calls.

--- src/frontend/lib/drupal.ts
+++ src/frontend/lib/drupal.ts
@@ -5,11 +5,11 @@ export const drupal = new DrupalClient(
   {
     auth: {
       clientId: process.env.DRUPAL_CLIENT_ID,
-      clientSecret: process.env.DRUPAL_CLIENT_SECRET,
-      'Authorization': `Basic ${btoa(process.env.BASIC_AUTH_USER + ':' + process.env.BASIC_AUTH_PASSWORD)}`
+      clientSecret: process.env.DRUPAL_CLIENT_SECRET
     },
     headers: {
       "Content-Type": "application/json",
+      'Authorization': `Basic ${btoa(process.env.BASIC_AUTH_USER + ':' + process.env.BASIC_AUTH_PASSWORD)}`
     },
   },
 );
