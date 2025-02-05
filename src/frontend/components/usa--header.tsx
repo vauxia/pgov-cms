adbabbf47a18bbd618c14dbcc29c1e67e6e7330a
Daniel <daniel.gading@civicactions.com>
2025-02-05 15:38:20 -0500

    Update pathauto and style breadcrumbs/header

--- src/frontend/components/usa--header.tsx
+++ src/frontend/components/usa--header.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import Image from "next/image";
 import { Header, PrimaryNav, Search, NavMenuButton, Title } from "@trussworks/react-uswds";
 import PGovLogo from '../assets/pgov-logo.svg';
+import USAGoalLogo from '../assets/usa-goals-logo.svg';
 
 export function USAHeader() {
   const pathname = usePathname();
@@ -20,25 +21,40 @@ export function USAHeader() {
   ];
 
   return(
-    <Header basic={true} showMobileOverlay={expanded}>
-       <div className="usa-nav-container padding-top-1">
-          <div className="usa-navbar">
-            <Title>
-              <Image
-                src={PGovLogo}
-                width={56}
-                height={56}
-                alt={"Performance.gov logo"}
-                priority
-                className=""
-              />
-            </Title>
-            <NavMenuButton onClick={onClick} label="Menu" />
-          </div>
-          <PrimaryNav items={menuItems} mobileExpanded={expanded} onToggleMobileNav={onClick}>
-            <Search size="small" onSubmit={(e) => {e.preventDefault(); console.log("Search");}} />
-          </PrimaryNav>
-        </div>
-    </Header>
+    <header className="site-header dark-blue-bg grid-row padding-x-3">
+      <Image
+        src={USAGoalLogo}
+        width={166}
+        height={60}
+        alt={"Performance.gov logo"}
+        priority
+        className="margin-right-6"
+      />
+      <nav className="grid-row flex-align-center">
+        <Link href="/" className={`usa-nav__link ${pathname === "/" ? "usa-current" : ""}`}>
+          <span>Explore</span>
+        </Link>
+      </nav>
+    </header>
+    // <Header basic={true} showMobileOverlay={expanded} className="dark-blue-bg">
+    //    <div className="usa-nav-container padding-top-1">
+    //       <div className="usa-navbar">
+    //         <Title>
+              // <Image
+              //   src={PGovLogo}
+              //   width={56}
+              //   height={56}
+              //   alt={"Performance.gov logo"}
+              //   priority
+              //   className=""
+              // />
+    //         </Title>
+    //         <NavMenuButton onClick={onClick} label="Menu" />
+    //       </div>
+    //       <PrimaryNav items={menuItems} mobileExpanded={expanded} onToggleMobileNav={onClick}>
+    //         <Search size="small" onSubmit={(e) => {e.preventDefault(); console.log("Search");}} />
+    //       </PrimaryNav>
+    //     </div>
+    // </Header>
   );
 }
