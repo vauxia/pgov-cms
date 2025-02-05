01b47bf512a0a8a2e87c0529bee3acc7e770a8d4
Allie Micka <allie@bluebird.io>
2025-02-05 11:29:49 -0600

    [PGOV-383] Refactor in order to reuse the data array, begin the pct complete calculation.

--- web/modules/custom/pgov_indicator/src/Entity/Storage/Indicator.php
+++ web/modules/custom/pgov_indicator/src/Entity/Storage/Indicator.php
@@ -18,21 +18,25 @@ class Indicator extends Storage {
    *   A list of measurements and performance indicator metadata.
    */
   public function getMeasurements() {
-
-    // Include some metadata from the Indicator.
-    $build = [
+    return [
       'target' => $this->get('field_target')->getString(),
       'dimension' => $this->get('field_dimension')->getString(),
       'keyness' => $this->get('field_keyness')->getString(),
-      'data' => [],
+      'data' => $this->getData(),
+      'progress' => $this->getProgress(),
     ];
+  }
 
-    // Format measurements into a simple array.
+  /**
+   * @return array
+   */
+  public function getData() {
+    $data = [];
     if ($this->hasField('field_measurements') && $measurements = $this->get('field_measurements')) {
       foreach ($measurements->referencedEntities() as $measurement) {
         $date = _pgov_indicator_get_date($measurement);
         $period = $measurement->get('field_period')->referencedEntities()[0];
-        $build['data'][$date] = [
+        $data[$date] = [
           'name' => $period->getName(),
           'date' => $date,
           'target' => $measurement->get('field_target_value')->getString(),
@@ -40,7 +44,21 @@ class Indicator extends Storage {
         ];
       }
     }
-    return $build;
+    return $data;
+  }
+
+  public function getProgress() {
+    $start = $current = $target = NULL;
+
+    foreach ($this->getData() as $row) {
+      // Start is the earliest ActualResult, so set it once.
+      if (is_null($start) && !empty($row['value'])) {
+        $start = $row['value'];
+      }
+    }
+
+    $progress = ($current - $start) / ($target - $start) * 100;
+    return $progress;
   }
 
 }
