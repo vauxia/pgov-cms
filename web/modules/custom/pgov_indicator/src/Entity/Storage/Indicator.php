4329812f973cfe78c9eeb77956c57b60ae99f9b2
Allie Micka <allie@bluebird.io>
2025-02-05 11:47:14 -0600

    [PGOV-383] Add pct complete calculation for measurement data.

--- web/modules/custom/pgov_indicator/src/Entity/Storage/Indicator.php
+++ web/modules/custom/pgov_indicator/src/Entity/Storage/Indicator.php
@@ -28,7 +28,10 @@ class Indicator extends Storage {
   }
 
   /**
+   * Return indicator measurements, as a simple array.
+   *
    * @return array
+   *   Target and actual values for measurements associated with this indicator.
    */
   public function getData() {
     $data = [];
@@ -47,18 +50,64 @@ class Indicator extends Storage {
     return $data;
   }
 
+  /**
+   * Returns progress to target, as a percentage.
+   *
+   * Based on the measurement values for this indicator, calculate the progress
+   * to measurement target.
+   *
+   * See https://docs.google.com/document/d/1b9x03CS2blmTmhEjiLgVf1gQDhcmT7ARuo6EOYpHpBY/edit?pli=1&tab=t.0
+   *
+   *   In short: the formula divides the absolute distance between the
+   *   start/current values by the absolute distance between the start/target
+   *   values for a given indicator.
+   * Handling Missing StartingResult
+   *   Since StartingResult is not explicitly stored, derive it dynamically,
+   *   ensuring that it resets when a new target is set:
+   * For the first-ever target set for a KPI
+   *   The earliest ActualResult (by StartDate) should be treated as the
+   *   StartingResult.
+   * For subsequent target changes
+   *   Whenever a TargetResult changes for a KPI, the first ActualResult
+   *   recorded after the change becomes the new StartingResult.
+   * Valid values only
+   *   Check whether values are valid. If a Target or StartingResult changes to
+   *   NA, it shouldn’t be treated as a new target or new actual - it simply
+   *   means the last valid Target or StartingResult value should persist.
+   *
+   * @return int
+   *   Progress to target, as a percentage (0-100)
+   */
   public function getProgress() {
-    $start = $current = $target = NULL;
+    $start = $target = $value = NULL;
 
     foreach ($this->getData() as $row) {
-      // Start is the earliest ActualResult, so set it once.
+      if ($row['target'] != $target && !empty($row['target'])) {
+        $target = $row['target'];
+
+        // When TargetResult changes, the first ActualResult recorded after
+        // the change becomes the new StartingResult.
+        $start = NULL;
+      }
+
+      // The earliest ActualResult (by Date) should be the StartingResult.
       if (is_null($start) && !empty($row['value'])) {
         $start = $row['value'];
       }
+
+      // Always update current, as long as the data is valid.
+      if (!empty($row['value'])) {
+        $value = $row['value'];
+      }
     }
 
-    $progress = ($current - $start) / ($target - $start) * 100;
-    return $progress;
+    // Return a percentage only if all numbers have been touched.
+    if (!empty($start) && !empty($value) && !empty($target)) {
+      return (($value - $start) / ($target - $start)) * 100;
+    }
+
+    // Return 0 on failure.
+    return 0;
   }
 
 }
