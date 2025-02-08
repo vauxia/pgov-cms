7eb58f546bd8b4ded26dc789cda7a6f0b62e7fb6
Allie Micka <allie@bluebird.io>
2025-02-07 20:15:44 -0600

    [PGOV-563] Fix editUrl path to make "Edit this page" links work.

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -41,7 +41,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/CivicActions/pgov-cms/tree/main/',
+            'https://github.com/CivicActions/pgov-cms/tree/main/docs/',
         },
         /** Will not use blog on this site.
         blog: {
